4220300681b420f7d84f29e3938f1dddab76beee
Enijar <TheEnijar@gmail.com>
2021-02-20 14:34:47 +0000

    fix(deployemnt): show home page

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import { AppWrapper } from "./styles";
 import Loading from "../loading/loading";
 
@@ -9,9 +8,7 @@ export default function App() {
   return (
     <AppWrapper>
       <React.Suspense fallback={<Loading />}>
-        <Switch>
-          <Route exact path="" component={Home} />
-        </Switch>
+        <Home />
       </React.Suspense>
     </AppWrapper>
   );
