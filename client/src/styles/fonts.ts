f3c737a90d36feea166cf0cdb94c21e2846344a3
Enijar <TheEnijar@gmail.com>
2021-02-20 14:27:34 +0000

    fix(deployment): paths

--- client/src/styles/fonts.ts
+++ client/src/styles/fonts.ts
@@ -4,8 +4,8 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
   @font-face {
     font-family: "Athelas";
-    src: url("/assets/fonts/athelas-bold.woff2") format("woff2"),
-    url("/assets/fonts/athelas-bold.woff") format("woff");
+    src: url("assets/fonts/athelas-bold.woff2") format("woff2"),
+    url("assets/fonts/athelas-bold.woff") format("woff");
     font-weight: bold;
     font-style: normal;
     font-display: swap;
@@ -13,8 +13,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-black.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-black.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-black.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-black.woff") format("woff");
     font-weight: 900;
     font-style: normal;
     font-display: swap;
@@ -22,8 +22,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-bold-italic.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-bold-italic.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-bold-italic.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-bold-italic.woff") format("woff");
     font-weight: bold;
     font-style: italic;
     font-display: swap;
@@ -31,8 +31,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-extra-light.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-extra-light.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-extra-light.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-extra-light.woff") format("woff");
     font-weight: 200;
     font-style: normal;
     font-display: swap;
@@ -40,8 +40,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-black-italic.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-black-italic.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-black-italic.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-black-italic.woff") format("woff");
     font-weight: 900;
     font-style: italic;
     font-display: swap;
@@ -49,8 +49,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-italic.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-italic.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-italic.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-italic.woff") format("woff");
     font-weight: normal;
     font-style: italic;
     font-display: swap;
@@ -58,8 +58,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-extra-light-italic.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-extra-light-italic.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-extra-light-italic.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-extra-light-italic.woff") format("woff");
     font-weight: 200;
     font-style: italic;
     font-display: swap;
@@ -67,8 +67,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-bold.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-bold.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-bold.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-bold.woff") format("woff");
     font-weight: bold;
     font-style: normal;
     font-display: swap;
@@ -76,8 +76,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-regular.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-regular.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-regular.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-regular.woff") format("woff");
     font-weight: normal;
     font-style: normal;
     font-display: swap;
@@ -85,8 +85,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-light.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-light.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-light.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-light.woff") format("woff");
     font-weight: 300;
     font-style: normal;
     font-display: swap;
@@ -94,8 +94,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-semi-bold-italic.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-semi-bold-italic.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-semi-bold-italic.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-semi-bold-italic.woff") format("woff");
     font-weight: 600;
     font-style: italic;
     font-display: swap;
@@ -103,8 +103,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-semi-bold.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-semi-bold.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-semi-bold.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-semi-bold.woff") format("woff");
     font-weight: 600;
     font-style: normal;
     font-display: swap;
@@ -112,8 +112,8 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: "Source Sans Pro";
-    src: url("/assets/fonts/source-sans-pro-light-italic.woff2") format("woff2"),
-    url("/assets/fonts/source-sans-pro-light-italic.woff") format("woff");
+    src: url("assets/fonts/source-sans-pro-light-italic.woff2") format("woff2"),
+    url("assets/fonts/source-sans-pro-light-italic.woff") format("woff");
     font-weight: 300;
     font-style: italic;
     font-display: swap;
